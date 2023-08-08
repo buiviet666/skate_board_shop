1b03d2b053abbc9f73acc543d38210760d5a432e
buiviet666 <loverbee01@gmail.com>
2023-08-08 15:02:28 +0700

    form web

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/css/nomalize.css">
+    <link rel="stylesheet" href="src/css/style.css">
+    <link rel="stylesheet" href="src/css/media.css">
+    <title>Skateboard life</title>
+    <link rel="shortcut icon" href="./src/img/favicon.png" type="image/x-icon">
+</head>
+<body>
+
+    <div id = "main">
+        <?php include "component_sidebar_left.php" ?>    
+
+        <?php include "component_content_right.php" ?>
+    </div>
+
+    <script src="src/javascript/index.js"></script>
+</body>
+</html>
