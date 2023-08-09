77534829656c7221342698221385b74a250a9b80
buiviet666 <loverbee01@gmail.com>
2023-08-09 15:30:48 +0700

    update sidebar left

---
+++ component_sidebar_left.php
@@ -0,0 +1,56 @@
+<div class="main_left_sidebar">
+    <header class="left_sidebar_position">
+        <div class="header_sticky">
+            <!-- header logo icon -->
+            <div class = "header_top">
+                <!-- p1 login signup -->
+                <div class="header_login_signup">
+                    <ul>
+                        <li class = "register">
+                            <a href = "#"><i class="fa-solid fa-user-plus"></i></a>
+                        </li>
+                        <li class = "login">
+                            <a href="#"><i class="fa-solid fa-user-large"></i></a>
+                        </li>
+                    </ul>
+                </div>
+
+                <!-- p2 logo -->
+                <h1 class = "header_logo">
+                    <a href ="#">
+                        <img src="./src/img/logo.avif" />
+                    </a>
+                </h1>
+            </div>
+
+            <!-- menu categories -->
+            <div class="grid--full header_menu_categories">
+                <div class ="wrapper">
+                    <div>
+                        <ul class = "header_menu_list">
+                            <li><a href="">HOME</a></li>
+                            <li><a href="">SHOP</a></li>
+                            <li><a href="">CATEGORIES</a></li>
+                            <li><a href="">ABOUT US</a></li>
+                            <li><a href="">CONTACT US</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <!-- icon categories -->
+            <ul class = "header_icon_categories">
+                <li>
+                    <a href = "#">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href = "#">
+                        <i class="fa-solid fa-cart-shopping"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </header>
+</div>
--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="src/css/nomalize.css">
-    <link rel="stylesheet" href="src/css/style.css">
+    <link rel='stylesheet' type='text/css' href='src/css/style.css'/>
     <link rel="stylesheet" href="src/css/media.css">
     <title>Skateboard life</title>
     <link rel="shortcut icon" href="./src/img/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;200;300;400;500;600;700&family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 </head>
+
 <body>
 
     <div id = "main">
@@ -19,4 +26,4 @@
 
     <script src="src/javascript/index.js"></script>
 </body>
-</html>
+</html> 
