2f481fd5bbd5455e502fa40b4cedd3f54070be7d
buiviet666 <loverbee01@gmail.com>
2023-08-14 08:47:04 +0700

    test

---
+++ component_content_right.php
@@ -0,0 +1,97 @@
+<div class = "main_right_content">
+    <div class = "right_content_position">
+        <!-- main content -->
+        <div>
+
+        </div>
+        <!-- footer content -->
+        <div class = "footer_main">
+            <!-- button toggle -->
+            <div id="slideToggle" class = "footer_btn_toggle">
+                Click me!!!
+            </div>
+            <!-- footer content -->
+            <footer class="footer_content_main">
+                <div class="wrapper">
+                    <div class="grid-uniform">
+                        <div class="footer_grid_item fourt_grid">
+                            <h4>Infomation</h4>
+                            <ul>
+                                <li>
+                                    <a href="#"><i class="fa-regular fa-paper-plane"></i>About Us</a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa-regular fa-paper-plane"></i>Privacy Policy</a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa-regular fa-paper-plane"></i>FAQ's</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="footer_grid_item fourt_grid">
+                            <h4>Support</h4>
+                            <ul>
+                                <li>
+                                    <a href="#"><i class="fa-regular fa-paper-plane"></i>Shop</a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa-regular fa-paper-plane"></i>Product</a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa-regular fa-paper-plane"></i>Help</a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa-regular fa-paper-plane"></i>Shipping option</a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa-regular fa-paper-plane"></i>Returns policy</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="footer_grid_item fourt_grid">
+                            <h4>Blogs</h4>
+                            <div class="home_blogs">
+                                <div class="odd">
+                                    <div class="home_blog_img">
+                                        <img src="src/img/blog-1_09544ddd-6a67-4ed8-b2f1-cee954d4c53c_small.jpg" alt="img_sk8"/>
+                                    </div>
+                                    <div class="home_blog_content">
+                                        <h3>Abandon with Rick McCrank</h3>
+                                        <p>This is your store's blog. You can use it to talk about new product...</p>
+                                    </div>
+                                </div>
+                                <div class="even">
+                                    <div>
+                                        <img src="src/img/blog-2_2035fd6d-8799-4aaf-874f-a4261e944db5_small.jpg" alt="img_sk8"/>
+                                    </div>
+                                    <div>
+                                        <h3>Classics with Rick</h3>
+                                        <p>This is your store's blog. You can use it to talk about new product...</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="footer_grid_item">
+                            <h4>Contact</h4>
+                            <p>
+                                <i class="fa-solid fa-house"></i>
+                                unknow where!!!
+                            </p>
+                            <p>
+                                <i class="fa-solid fa-envelope"></i>
+                                info@gmail.com
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <div class="footer_copyrightend">
+                <div class="copyright">
+                    <p>
+                        Copyright © 2023, Skateboards <a href="#">Powered by 6</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
