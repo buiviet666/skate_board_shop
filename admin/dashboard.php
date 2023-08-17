7b4037978094b94bb5452441cd10a89b7970b967
buiviet666 <loverbee01@gmail.com>
2023-08-17 16:42:09 +0700

    update page

---
+++ admin/dashboard.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="src/css/admincss.css">
+</head>
+<body>
+    
+    <h1>hi this is admin page</h1>
+
+    <script src="src/javascript/adminjs.js"></script>
+</body>
+</html>
