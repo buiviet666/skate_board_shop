7cad541fdbbf456bf66cbc4d92087f4141780f86
buiviet666 <loverbee01@gmail.com>
2023-08-23 11:42:34 +0700

    update page about us

--- about_us.php
+++ about_us.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="src/css/nomalize.css">
     <link rel='stylesheet' type='text/css' href='src/css/style.css'/>
     <link rel="stylesheet" href="src/css/media.css">
+    <link rel="stylesheet" href="src/css/components.css">
     <title>Skateboard life</title>
     <link rel="shortcut icon" href="./src/img/favicon.png" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -26,8 +27,237 @@
         <div class = "main_right_content">
             <div class = "right_content_position">
                 <!-- main content -->
+                <div class="about-us">
+                    <div class="about-section">
+                        <div>
+                            <div>
+                                <div>
+                                    <img src="./src/img/abt_welcome_1.jpg">
+                                </div>
+                                <div>
+                                    noi dung
+                                    <div>
+                                        <h3>Multipurpose store</h3>
+                                        <p>
+                                            There are many variations of passages of Lorem Ipsum available, but the majority 
+                                            have suffered alteration in some form, by injected humour, or randomised words 
+                                            which don't look even slightly believable. Lorem Ipsum available, but the majority
+                                                have suffered alteration in some form. Vivamus adipiscing lobortis sagittis. Nullam 
+                                                tempus mauris dolor, ac malesuada arcu. Praesent dolor quam, tincidunt in sollicitudin 
+                                                sit amet, volutpat sed velit. Nullam non neque ipsum.Lorem ipsum dolor sit amet, 
+                                                consectetur adipiscing elit. Morbi euismod diam eu arcu volutpat ut adipiscing sem auctor.
+                                                Vivamus adipiscing lobortis sagittis. Nullam tempus mauris dolor, ac malesuada arcu.
+                                        </p>
+                                        <a>Read More</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="about-section">
+                        <div>
+                            <div>
+                                <div class="">
+                                    item1
+                                    <img>
+                                    <div>
+                                        <span>STYLE</span>
+                                    </div>
+                                </div>
+                                <div class="">
+                                    item2
+                                    <img>
+                                    <div>
+                                        <span>COMFORT</span>
+                                    </div>
+                                </div>
+                                <div class="">
+                                    item3
+                                    <img>
+                                    <div>
+                                        <span>PRECISION</span>
+                                    </div>
+                                </div>
+                                <div class="">
+                                    item4
+                                    <img>
+                                    <div>
+                                        <span>DURABLE</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="about-section">
+                        <div>
+                            <div></div>
+                            <div>
+                                <div>
+                                    <div>
+                                        <div>
+                                            <div>
+                                                <i class="fa-regular fa-gem"></i>
+                                            </div>
+                                            <h5>TOTALLY RESPONSIVE</h5>
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.Suspendisse urna nibh, 
+                                                viverra non, semper suscipit, posuere a, pede.
+                                            </p>
+                                        </div>
+                                        <div>
+                                            <div>
+                                                <i class="fa-solid fa-graduation-cap"></i>
+                                            </div>
+                                            <h5>DIFFERENT HEADER TYPES</h5>
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.Suspendisse urna nibh, 
+                                                viverra non, semper suscipit, posuere a, pede.
+                                            </p>
+                                        </div>
+                                        <div>
+                                            <div>
+                                                <i class="fa-solid fa-headphones"></i>
+                                            </div>
+                                            <h5>HIGHLY CUSTOMIZABLE</h5>
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.Suspendisse urna nibh, 
+                                                viverra non, semper suscipit, posuere a, pede.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="about-section">
+                        <div>
+                            <div>
+                                <div>
+                                    <h3>Our Team</h3>
+                                    <div>
+                                        <div>
+                                            <p>
+                                            Nullam ultrices cursus orci id maximus. Etiam ultricies, turpis ac accumsan volutpat, 
+                                            nisi nisi fermentum orci, eu pulvinar justo sapien ut orci. Lorem ipsum dolor sit amet, 
+                                            consectetur adipiscing elit. Sed ut elit vel lectus fringilla molestie ac et turpis. Ut 
+                                            sed tempus nisi. In eleifend vel velit nec pulvinar.
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <ul>
+                                            <li>
+                                                <div>
+                                                    <div>
+                                                        <img>
+                                                    </div>
+                                                    <div>
+                                                        hover mouse
+                                                    </div>
+                                                </div>
+                                                <div>
+                                                    hover item
+                                                </div>
+                                            </li>
+                                            <li>
+                                                <div>
+                                                    <div>
+                                                        <img>
+                                                    </div>
+                                                    <div>
+                                                        hover mouse
+                                                    </div>
+                                                </div>
+                                                <div>
+                                                    hover item
+                                                </div>
+                                            </li>
+                                            <li>
+                                                <div>
+                                                    <div>
+                                                        <img>
+                                                    </div>
+                                                    <div>
+                                                        hover mouse
+                                                    </div>
+                                                </div>
+                                                <div>
+                                                    hover item
+                                                </div>
+                                            </li>
+                                            <li>
+                                                <div>
+                                                    <div>
+                                                        <img>
+                                                    </div>
+                                                    <div>
+                                                        hover mouse
+                                                    </div>
+                                                </div>
+                                                <div>
+                                                    hover item
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="about-section">
+                        <div>
+                            <div>
+                                <div>
+                                    <div>
+                                        <div>
+                                            <i class="fa-solid fa-briefcase"></i>
+                                        </div>
+                                        <div>
+                                            <h4>548</h4>
+                                            <p>
+                                                PROJECT COMPLETED
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div>
+                                            <i class="fa-regular fa-clock"></i>
+                                        </div>
+                                        <div>
+                                            <h4>1548</h4>
+                                            <p>
+                                                WORKING HOURS
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div>
+                                            <i class="fa-regular fa-star"></i>
+                                        </div>
+                                        <div>
+                                            <h4>748</h4>
+                                            <p>
+                                                POST FEEDBACKS
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div>
+                                        <h3>Promo Ads</h3>
+                                        <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.Veritatis et quasi architecto 
+                                        beatae vitae dicta sunt explicabo.Lorem ipsum dolor sit amet, consectetur adipiscing 
+                                        elit.Vivamus adipiscing lobortis sagittis.
+                                        </p>
+                                        <a>View Details</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 
-
                 <!-- footer content -->
                 <?php include "component_footer.php" ?>
             </div>
