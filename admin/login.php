d9c4a8436e934a93e0e99c4485f66f4ce644723a
buiviet666 <loverbee01@gmail.com>
2023-09-05 23:14:29 +0700

    update login

---
+++ admin/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/css/admincss.css">
+    <link rel="stylesheet" href="../src/css/nomalize.css">
+    <title>Document</title>
+</head>
+<body>
+    
+    <div class="login" onclick="onclick">
+        <div class="login_top"></div>
+        <div class="login_bottom"></div>
+        <div class="login_center">
+            <form>
+                <h2>Please Sign In</h2>
+                <input type="email" placeholder="email"/>
+                <input type="password" placeholder="password"/>
+                <input type="submit" value="đăng nhập">
+            </form>
+        </div>
+    </div>
+
+</body>
+</html>
