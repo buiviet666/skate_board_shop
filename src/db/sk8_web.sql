7c6a84bad20b6b6d4dc95b04a059496248ed89de
buiviet666 <loverbee01@gmail.com>
2023-09-14 10:45:26 +0700

    update db

--- src/db/sk8_web.sql
+++ src/db/sk8_web.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th8 08, 2023 lúc 05:49 AM
+-- Thời gian đã tạo: Th9 14, 2023 lúc 05:44 AM
 -- Phiên bản máy phục vụ: 10.4.28-MariaDB
 -- Phiên bản PHP: 8.2.4
 
@@ -31,11 +31,10 @@ CREATE TABLE `donhang` (
   `id_donhang` int(11) NOT NULL,
   `address_donhang` text NOT NULL,
   `phone_donhang` varchar(15) NOT NULL,
+  `email_donhang` varchar(50) NOT NULL,
   `trangthai_donhang` enum('Đang xử lý','Đang giao hàng','Đã hủy','Đã hoàn thành') NOT NULL,
   `soluong_sanpham` int(11) NOT NULL,
-  `id_product` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
-  `id_hoadon` int(11) NOT NULL
+  `id_user` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -46,7 +45,8 @@ CREATE TABLE `donhang` (
 
 CREATE TABLE `hoadon` (
   `id_hoadon` int(11) NOT NULL,
-  `ngaylap_hoadon` date NOT NULL,
+  `id_donhang` int(11) NOT NULL,
+  `ngaylap_hoadon` date NOT NULL DEFAULT current_timestamp(),
   `tongtien` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -61,8 +61,10 @@ CREATE TABLE `product` (
   `name_product` varchar(255) NOT NULL,
   `desc_product` text NOT NULL,
   `price_product` float NOT NULL,
-  `img_product` varchar(50) NOT NULL,
-  `name_manufacturer` varchar(50) NOT NULL
+  `img_product` varchar(255) NOT NULL,
+  `type_product` varchar(50) NOT NULL,
+  `number_product` int(11) NOT NULL,
+  `id_producer` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -76,6 +78,15 @@ CREATE TABLE `roles` (
   `ten_roles` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Đang đổ dữ liệu cho bảng `roles`
+--
+
+INSERT INTO `roles` (`id_roles`, `ten_roles`) VALUES
+(1, 'admin'),
+(2, 'employee'),
+(3, 'customer');
+
 -- --------------------------------------------------------
 
 --
@@ -86,14 +97,23 @@ CREATE TABLE `users` (
   `id_user` int(11) NOT NULL,
   `ten_user` varchar(255) NOT NULL,
   `gioitinh_user` enum('Nam','Nữ','Khác','') NOT NULL,
-  `usernam_user` varchar(255) NOT NULL,
+  `username_user` varchar(255) NOT NULL,
   `password_user` varchar(255) NOT NULL,
   `phone_user` varchar(15) NOT NULL,
   `email_user` varchar(255) NOT NULL,
   `address_user` text NOT NULL,
   `id_roles` int(11) NOT NULL,
-  `token` int(11) NOT NULL
+  `token` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `users`
+--
+
+INSERT INTO `users` (`id_user`, `ten_user`, `gioitinh_user`, `username_user`, `password_user`, `phone_user`, `email_user`, `address_user`, `id_roles`, `token`) VALUES
+(9, 'khach hang 1', '', 'customer', '123', '0379583754', 'kh@gmail.com', 'Ha Noi', 3, NULL),
+(10, 'quan ly 1', '', 'admin', '123456', '', 'admin@gmail.com', 'Ha Noi', 1, NULL),
+(11, 'nhan vien 1', 'Nam', 'employee', '123', '214124124', 'employee@gmail.com', 'Son La', 3, 0);
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -104,21 +124,21 @@ CREATE TABLE `users` (
 --
 ALTER TABLE `donhang`
   ADD PRIMARY KEY (`id_donhang`),
-  ADD KEY `id_user` (`id_user`),
-  ADD KEY `id_hoadon` (`id_hoadon`),
-  ADD KEY `id_product` (`id_product`);
+  ADD KEY `id_user` (`id_user`);
 
 --
 -- Chỉ mục cho bảng `hoadon`
 --
 ALTER TABLE `hoadon`
-  ADD PRIMARY KEY (`id_hoadon`);
+  ADD PRIMARY KEY (`id_hoadon`),
+  ADD KEY `id_donhang` (`id_donhang`);
 
 --
 -- Chỉ mục cho bảng `product`
 --
 ALTER TABLE `product`
-  ADD PRIMARY KEY (`id_product`);
+  ADD PRIMARY KEY (`id_product`),
+  ADD KEY `id_producer` (`id_producer`);
 
 --
 -- Chỉ mục cho bảng `roles`
@@ -141,31 +161,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT cho bảng `donhang`
 --
 ALTER TABLE `donhang`
-  MODIFY `id_donhang` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_donhang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT cho bảng `hoadon`
 --
 ALTER TABLE `hoadon`
-  MODIFY `id_hoadon` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_hoadon` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT cho bảng `product`
 --
 ALTER TABLE `product`
-  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT cho bảng `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `id_roles` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_roles` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT cho bảng `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Các ràng buộc cho các bảng đã đổ
@@ -175,9 +195,19 @@ ALTER TABLE `users`
 -- Các ràng buộc cho bảng `donhang`
 --
 ALTER TABLE `donhang`
-  ADD CONSTRAINT `donhang_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `donhang_ibfk_2` FOREIGN KEY (`id_hoadon`) REFERENCES `hoadon` (`id_hoadon`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `donhang_ibfk_3` FOREIGN KEY (`id_product`) REFERENCES `product` (`id_product`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `donhang_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Các ràng buộc cho bảng `hoadon`
+--
+ALTER TABLE `hoadon`
+  ADD CONSTRAINT `hoadon_ibfk_2` FOREIGN KEY (`id_donhang`) REFERENCES `donhang` (`id_donhang`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Các ràng buộc cho bảng `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`id_producer`) REFERENCES `producer` (`id_producer`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Các ràng buộc cho bảng `users`
